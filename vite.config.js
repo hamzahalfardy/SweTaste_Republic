335d86482575a4b35c02a0f80530a26de91cdeed
hamzahalfardy <azesodonibabaomebi@gmail.com>
2025-09-23 23:22:15 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Food-Order/',
+  build: {
+    outDir: 'docs'
+  },
   server:{
     port : 3000 
   }
