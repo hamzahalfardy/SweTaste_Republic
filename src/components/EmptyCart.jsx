71a91b8f7eb1af8fb94c6b81b451f09fc672fdf3
hamzahalfardy <160720239+hamzahalfardy@users.noreply.github.com>
2025-10-08 04:50:21 +0700

    Add files via upload

---
+++ src/components/EmptyCart.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const EmptyCart = () => {
+  return (
+    <div className="flex flex-col items-center">
+          <img
+            src={`${import.meta.env.BASE_URL}images/illustration-empty-cart.svg`}
+            alt="empty cart"
+            className="w-40 mb-2"
+          />
+          
+          <p className="text-gray-600">Items will appear here...</p>
+      </div>
+  )
+}
+
+export default EmptyCart
---
+++ src/components/PopUpOrders.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const PopUpOrders = ({orderSummary, closePopup}) => {
+
+  return (
+            <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <div className="bg-white rounded-xl p-6 shadow-lg text-center w-96">
+            <img
+              src={`${import.meta.env.BASE_URL}images/icon-order-confirmed.svg`}
+              alt=""
+              className="w-8"
+            />
+            <h2 className="text-2xl font-bold text-gray-700 mb-3 w-60 text-left">
+              Order Confirmed!
+            </h2>
+            <p className="text-gray-700 mb-4">We hope you enjoy your Food</p>
+
+            {/* use orderSummary here instead of cart */}
+            <div className="max-h-60 overflow-y-auto text-left mb-4 border-t pt-2">
+              {orderSummary.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex justify-between items-center mb-2 border-b"
+                >
+                  <div className="flex gap-2">
+                    <img
+                      src={`${import.meta.env.BASE_URL}${item.image.thumbnail}`}
+                      alt={item.name}
+                      className="w-10 h-10 rounded"
+                    />
+                    <div>
+                      <p className="text-sm">{item.name}</p>
+                      <p className="text-red-700 font-bold ">
+                        {item.quantity}x{" "}
+                        <span className="text-gray-600 ml-6">
+                          @${item.price}
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                  <span className="font-medium">
+                    ${(item.price * item.quantity).toFixed(2)}
+                  </span>
+                </div>
+              ))}
+            </div>
+
+            {/* Total */}
+            <div className="flex justify-between font-bold text-lg mb-4">
+              <span>Total:</span>
+              <span>
+                $
+                {orderSummary
+                  .reduce((sum, item) => sum + item.price * item.quantity, 0)
+                  .toFixed(2)}
+              </span>
+            </div>
+
+            <button
+              onClick={closePopup}
+              className="bg-amber-600 text-white px-6 py-2 rounded-lg font-bold hover:bg-amber-700 transition"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+
+  )
+}
+
+export default PopUpOrders
