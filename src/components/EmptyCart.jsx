24fe1e6185a0290c0392aa1574319ede4b435eee
hamzahalfardy <160720239+hamzahalfardy@users.noreply.github.com>
2025-10-24 14:28:50 +0700

    Refactor EmptyCart component for improved readability

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const EmptyCart = () => {
   return (
     <div className="flex flex-col items-center">
-          <img
-            src={public/images/illustration-empty-cart.svg
-}
-            alt="empty cart"
-            className="w-40 mb-2"
-          />
-          
-          <p className="text-gray-600">Items will appear here...</p>
-      </div>
-  )
-}
+      <img
+        src={"/illustration-empty-cart.svg"}
+        alt="empty cart"
+        className="w-40 mb-2"
+      />
 
-export default EmptyCart
+      <p className="text-gray-600">Items will appear here...</p>
+    </div>
+  );
+};
 
+export default EmptyCart;
